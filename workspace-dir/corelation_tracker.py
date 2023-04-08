0c9ba09a5b91a8661f00f37b27200a077c554489
AMihevc <mihevc.anze@gmail.com>
2023-04-09 00:39:52 +0200

    Not working version with filter function

--- workspace-dir/corelation_tracker.py
+++ workspace-dir/corelation_tracker.py
@@ -12,6 +12,16 @@ class MosseSimple(Tracker):
     def make_fft_patch(self, image):
         # get the patch
         patch, crop_mask = get_patch(image, self.position, self.size)
+        # print(patch.shape)
+        # print(self.size)
+
+        if (patch.shape[0] != self.size[1] or patch.shape[1] != self.size[0]):
+            print("Error: patch size is not correct")
+            print("Patch shape:", patch.shape)
+            print("Expected size:", self.size)
+            print("Position:", self.position)
+            print("Image shape:", image.shape)
+            #exit()
 
         #convert to grayscale
         patch = cv2.cvtColor(patch, cv2.COLOR_BGR2GRAY)
@@ -24,10 +34,13 @@ class MosseSimple(Tracker):
         
         return patch
     
+    def make_filter(self, given_patch):
+        return (self.fft_gaussian_peak * np.conj(given_patch)) / ((self.patch * np.conj(given_patch)) + self.lamda)
+    
     def initialize(self, image, region): #initialize the tracker
         #parameters (majbe should be able to change them with arguments)
         self.enlargment_factor = 1.0
-        self.alpha = 0.125
+        self.alpha = 0.2
         self.sigma = 2.0
         self.lamda = 0.000001
 
@@ -42,6 +55,7 @@ class MosseSimple(Tracker):
             self.original_size = (self.original_size[0] + 1, self.original_size[1])
         if self.original_size[1] % 2 == 0:
             self.original_size = (self.original_size[0], self.original_size[1] + 1)
+       
 
         # define the size with enlargement
         self.size = (round(region[2] * self.enlargment_factor), round(region[3] * self.enlargment_factor))
@@ -50,9 +64,13 @@ class MosseSimple(Tracker):
             self.size = (self.size[0] + 1, self.size[1])
         if self.size[1] % 2 == 0:
             self.size = (self.size[0], self.size[1] + 1)
+        # print("size:")
+        # print(self.size)
 
         # define the cosine window
         self.cosine_window = create_cosine_window(self.size)
+        # print("cosine window:")
+        # print(self.cosine_window.shape)
 
         # define the gaussian peak
         self.gaussian_peak = create_gauss_peak(self.size, self.sigma)
@@ -62,7 +80,7 @@ class MosseSimple(Tracker):
         self.patch = self.make_fft_patch(image)
 
         # define the filter
-        self.filter = (self.fft_gaussian_peak * np.conj(self.patch)) / ((self.patch * np.conj(self.patch)) + self.lamda)
+        self.filter = self.make_filter(self.patch)
 
     
     def track(self, image): #track the object in the image
@@ -73,16 +91,24 @@ class MosseSimple(Tracker):
         response = np.fft.ifft2(self.filter * fft_patch)
 
         # find the peak
-        peak = np.unravel_index(np.argmax(response), response.shape) #is a tuple (x,y)
-
+        peak_y, peak_x  = np.unravel_index(np.argmax(response), response.shape) #is a tuple (y,x)
 
         # update the position
-        self.position = (self.position[0] + (peak[0] - self.size[0] / 2), self.position[1] + (peak[1] - self.size[1] / 2)) # this is the center of the patch
-        #TODO check if this is correct from slides you might not need to subtract the size
+        # self.position = (self.position[0] + (peak[0] - self.size[0] / 2), self.position[1] + (peak[1] - self.size[1] / 2)) # this is the center of the patch
+        #Todo-Done check if this is correct from slides you might not need to subtract the size (it was not correct )
+        # from slides: 
+        if (peak_x > self.size[0] / 2):
+            peak_x = peak_x - self.size[0]
+
+        if (peak_y > self.size[1] / 2):
+            peak_y = peak_y - self.size[1]
+
+        self.position = (self.position[0] + peak_x, self.position[1] + peak_y)
+        
 
         # update the filter
         new_patch = self.make_fft_patch(image)
-        self.filter = ((self.filter * (1 - self.alpha)) + ((self.fft_gaussian_peak * np.conj(self.patch)) / ((self.patch * np.conj(self.patch)) + self.lamda)) * self.alpha) #TODO check if this is correct and could be preatyer 
+        self.filter = (self.filter * (1 - self.alpha) + (self.make_filter(new_patch) * self.alpha)) 
 
         # return the new position as list
         return [self.position[0] - self.original_size[0] / 2, self.position[1] - self.original_size[1] / 2, self.original_size[0], self.original_size[1]]
